f807abcbdc1356aba09439cb2f8857f4e08b9b04
r2dong <r2dong@yahoo.com>
2018-01-11 05:41:20 -0800

    support writing csv for multiple sections, not tested

--- introToCS.py
+++ introToCS.py
@@ -1,10 +1,29 @@
 from fileUtility import *
+from inspect import signature
 
-stuInfoColNum = 3 # largest column number containing student info on the left
+stuInfoColNum = 3 # largest column number containing student info from the left
 insertCol = stuInfoColNum + 1 # colum number to write student scores
-IDCol = 2 
+IDCol = 2 # column number where hawkID is in
+numArg = 3 # number of arguments needed for writeSectionOutput
 
+
+# writes csvs for all sections to re-upload to ICON
+# Inputs:
+# stfResults - <[[studentFile]]> litst of student files collected in an outer
+#              list, ordered by sections
+# optArgs - <[[str, str, str]]> inside tuple, elements are
+#           respectively name of output file, hwID, and file name of csv
+#           downloaded from ICON. Follows same order as stfResults
 def writeOutput(stfResults, optArgs):
+    if len(stfResults) != len(optArgs):
+        raise Exception("Error in section information")
+    else:
+        optArgs = cmdArgToInput(optArgs)
+        for result, arg in zip(stfResults, optArgs):
+            writeSectionOutput(result, arg)
+
+# writes a csv to re-upload to ICON for one section
+def writeSectionOutput(stfResults, optArgs):
     
     outFileName = optArgs[0] # consider removing, user really need no control over this
     hwID = optArgs[1]
@@ -64,6 +83,19 @@ def writeScore(point, ID, matrix):
         if row[IDCol] == ID:
             row[insertCol] = point
             break
-    
-    
-        
+
+# convert arguments from cmdLine format to that needed by writeOutput
+# Inputs:
+# cmdArgs - <[str]> raw command line arguments recieved
+def cmdArgToInput(cmdArgs):
+    if len(cmdArgs) % numArg != 0:
+        errMsg = "Incorrect number of inputs to IntroToCS writeOutput"
+        raise Exception(errMsg)
+    else:
+        formattedArgs = []
+        for index in range(0, len(cmdArgs) / numArg):
+            sectionArgList = []
+            for argIndex in range(0, numArg):
+                sectionArgList.append(cmdArgs[index + argIndex])
+            formattedArgs.append(sectionArgList)
+        return formattedArgs
